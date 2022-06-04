cfa439634bf87dd4eaba40a4425d20d52aafffbf
lmbelo <lucas.belo@live.com>
2022-06-03 18:15:58 -0700

    Creating the default script when a new project is created

--- source/services/Services.Project.pas
+++ source/services/Services.Project.pas
@@ -9,14 +9,18 @@ uses
 type
   TProjectService = class(TInterfacedObject, IProjectServices)
   private
+    function GetBasePath(): string;
+    function GetProjectFilesPath(const AProjectName: string): string;
     function TestProject(const AModel: TProjectModel): boolean;
   public
-    function CreateProject(const AApplicationName: string): TProjectModel;
+    function CreateProject(const AApplicationName: string;
+      const AAddMainScript: boolean = true): TProjectModel;
     procedure SaveProject(const AProject: TProjectModel);
     function LoadProject(const AApplicationName: string): TProjectModel;
     function ListProjects(): TArray<string>;
     function HasProject(const AApplicationName: string): boolean;
 
+    function AddMainScriptFile(const AModel: TProjectModel): string;
     function AddScriptFile(const AModel: TProjectModel;
       const AFilePath: string): boolean;
     procedure RemoveScriptFile(const AModel: TProjectModel;
@@ -30,6 +34,16 @@ uses
   Storage.Default;
 
 { TProjectService }
+
+function TProjectService.GetBasePath: string;
+begin
+  Result := TPath.Combine(ExtractFilePath(ParamStr(0)), 'files');
+end;
+
+function TProjectService.GetProjectFilesPath(const AProjectName: string): string;
+begin
+  Result := TPath.Combine(GetBasePath(), AProjectName);
+end;
 
 function TProjectService.HasProject(const AApplicationName: string): boolean;
 begin
@@ -47,9 +61,13 @@ begin
   Result := HasProject(AModel.ApplicationName);
 end;
 
-function TProjectService.CreateProject(const AApplicationName: string): TProjectModel;
+function TProjectService.CreateProject(const AApplicationName: string;
+  const AAddMainScript: boolean): TProjectModel;
 begin
   Result := TProjectModel.Create(AApplicationName);
+
+  if AAddMainScript then
+    AddMainScriptFile(Result);
 end;
 
 function TProjectService.ListProjects: TArray<string>;
@@ -83,6 +101,52 @@ begin
   var LStorage := TDefaultStorage<TProjectModel>.Make();
   if not LStorage.LoadModel(Result, String.Empty, AApplicationName) then
     raise Exception.CreateFmt('Project %s not found.', [AApplicationName]);
+end;
+
+function TProjectService.AddMainScriptFile(const AModel: TProjectModel): string;
+const
+  SCRIPT_TEXT =
+      'from delphifmx import *'
+    + #13#10
+    + #13#10
+    + 'MainForm = Form(Application)'
+    + #13#10
+    + 'MainForm.SetProps(Caption = "Hello World")'
+    + #13#10
+    + 'msg = Label(MainForm)'
+    + #13#10
+    + 'msg.SetProps(Parent = MainForm,'
+    + #13#10
+    + '    Text = "Hello Python from Delphi FMX",'
+    + #13#10
+    + '    Position = Position(PointF(50, 50)),'
+    + #13#10
+    + '    Width = 300)'
+    + #13#10
+    + 'MainForm.Show()';
+begin
+  var LProjectFilesFolder := GetProjectFilesPath(AModel.ApplicationName);
+  if not TDirectory.Exists(LProjectFilesFolder) then
+    TDirectory.CreateDirectory(LProjectFilesFolder);
+
+  var LMainScriptPath := TPath.Combine(
+    GetProjectFilesPath(AModel.ApplicationName),
+    'main.py');
+
+  if not TFile.Exists(LMainScriptPath) then begin
+    with TFile.Create(LMainScriptPath) do begin
+      try
+        WriteData(
+          TEncoding.UTF8.GetBytes(SCRIPT_TEXT),
+          TEncoding.UTF8.GetByteCount(SCRIPT_TEXT));
+      finally
+        Free();
+      end;
+    end;
+  end;
+
+  //Save the script file in the model files
+  AddScriptFile(AModel, LMainScriptPath);
 end;
 
 function TProjectService.AddScriptFile(const AModel: TProjectModel;
