48b3c14b8c5eea5f52e1dcdfb6477372595de4d7
Priyatham <kunarapupriyatham@gmail.com>
2022-06-08 22:14:23 +0530

    Update frames_init.py

--- Samples/Frames/frames_init.py
+++ Samples/Frames/frames_init.py
@@ -15,9 +15,9 @@ Application.Initialize()
 
 myForm = Form(Application)
 
-myFrame1 = Frame1(myForm, lbl="Top Aligned Frame")
+myFrame1 = Frame1(myForm, lbl="Top")
 myFrame1.SetProps(Parent=myForm, Align="Top")
-myFrame2 = Frame1(myForm, lbl="Bottom Aligned Frame")
+myFrame2 = Frame1(myForm, lbl="Bottom")
 myFrame2.SetProps(Parent=myForm, Align="Top")
 
 Application.MainForm = myForm
