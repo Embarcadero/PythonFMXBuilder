a86c03f35e94b4cd74f3d16121bb736d18958d8f
lmbelo <lucas.belo@live.com>
2023-07-27 10:22:30 -0300

    TabItem enhancements

--- source/views/frame/editor/Frame.Editor.TabItem.pas
+++ source/views/frame/editor/Frame.Editor.TabItem.pas
@@ -4,59 +4,33 @@ interface
 
 uses
   System.Rtti, System.Classes, System.Types, FMX.Types, FMX.Controls, FMX.TabControl,
-  Builder.Chain;
+  Builder.Types, Builder.Chain, Builder.TabControl;
 
 type
-  ITextEditor = interface
-    ['{52EDE988-F0AF-44B2-8445-1BD8960DD3C2}']
-    function GetBreakpoints(): TArray<integer>;
-    procedure SetBreakpoints(ABreakpoints: TArray<integer>);
-    function GetActiveLine(): integer;
-    procedure SetActiveLine(AActiveLine: integer);
-    function GetShowActiveLine(): boolean;
-    procedure SetShowActiveLine(AShowActiveLine: boolean);
-
-    procedure LoadFromFile(const AFileName: string);
-    procedure Save();
-    procedure SaveTo(const AFileName: string);
-
-    property Breakpoints: TArray<integer> read GetBreakpoints write SetBreakpoints;
-    property ActiveLine: integer read GetActiveLine write SetActiveLine;
-    property ShowActiveLine: boolean read GetShowActiveLine write SetShowActiveLine;
-  end;
-
   TControlClass = class of TControl;
 
   TEditorTabItem = class(TTabItem)
   private
     class var FDefaultEditorClass: TControlClass;
   private
-    FFileName: string;
-    FCanClose: boolean;
     FEditor: TControl;
     //Events
     FSaveState: IDisconnectable;
     FRenameFile: IDisconnectable;
-    function GetCloseControl(): TControl;
-    procedure OnCloseTab(Sender: TObject);
+    FEditorChanged: IDisconnectable;
     procedure CreateEditor();
   protected
-    procedure ApplyStyle; override;
-    procedure SetText(const Value: string); override;
-    function GetData: TValue; override;
-    procedure SetData(const Value: TValue); override;
-
     function GetTextEditor(): ITextEditor;
-    procedure DoClose(); virtual;
+    procedure InternalSave();
+    procedure DoAskToSaveChanges();
+    procedure DoClose(); override;
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy(); override;
 
-    procedure LoadFile(const AFileName: string);
+    procedure LoadFile(const AFileName: string; const AEditing: boolean);
 
-    property FileName: string read FFileName write FFileName;
     property TextEditor: ITextEditor read GetTextEditor;
-    property CanClose: boolean read FCanClose write FCanClose;
 
     class property DefaultEditorClass: TControlClass
       read FDefaultEditorClass write FDefaultEditorClass;
@@ -65,7 +39,8 @@ type
 implementation
 
 uses
-  System.SysUtils, System.IOUtils;
+  System.SysUtils, System.IOUtils, System.UITypes,
+  FMX.Dialogs, FMX.DialogService;
 
 { TEditorTabItem }
 
@@ -78,20 +53,38 @@ begin
     begin
       if (AEventNotification.Body.SaveState = TSaveState.Save) then begin
         if (TabControl.ActiveTab = Self) then
-          TextEditor.Save();
+          InternalSave();
       end else if (AEventNotification.Body.SaveState = TSaveState.SaveAll) then
-        TextEditor.Save();
+        InternalSave();
     end);
   FRenameFile := TGlobalBuilderChain.SubscribeToEvent<TRenameFileEvent>(
     procedure(const AEventNotification: TRenameFileEvent)
     begin
-      if (FFileName = AEventNotification.Body.OldFilePath) then begin
+      if (TextEditor.FileName = AEventNotification.Body.OldFilePath) then begin
         TextEditor.SaveTo(AEventNotification.Body.NewFilePath);
         TThread.Synchronize(TThread.Current, procedure() begin
-          LoadFile(AEventNotification.Body.NewFilePath);
+          LoadFile(AEventNotification.Body.NewFilePath, false);
         end);
       end;
     end);
+  FEditorChanged := TGlobalBuilderChain.SubscribeToEvent<TEditorChangedEvent>(
+    procedure(const AEventNotification: TEditorChangedEvent)
+    begin
+      if (AEventNotification.Body.TextEditor = TextEditor) then begin
+        var LModified := AEventNotification.Body.Modified;
+        TThread.Queue(TThread.Current, procedure() begin
+          Modified := LModified;
+        end);
+      end;
+    end);
+end;
+
+destructor TEditorTabItem.Destroy;
+begin
+  FEditorChanged.Disconnect();
+  FRenameFile.Disconnect();
+  FSaveState.Disconnect();
+  inherited;
 end;
 
 procedure TEditorTabItem.CreateEditor;
@@ -101,82 +94,48 @@ begin
   FEditor.Align := TAlignLayout.Client;
 end;
 
-destructor TEditorTabItem.Destroy;
-begin
-  FRenameFile.Disconnect();
-  FSaveState.Disconnect();
-  inherited;
-end;
-
-procedure TEditorTabItem.ApplyStyle;
-begin
-  inherited;
-  var LControl := GetCloseControl();
-  if Assigned(LControl) then begin
-    LControl.OnClick := OnCloseTab;
-    LControl.Visible := FCanClose;
-  end;
-end;
-
-function TEditorTabItem.GetData: TValue;
-begin
-  Result := FFileName;
-end;
-
 function TEditorTabItem.GetTextEditor: ITextEditor;
 begin
   Result := FEditor as ITextEditor;
 end;
 
-procedure TEditorTabItem.LoadFile(const AFileName: string);
+procedure TEditorTabItem.InternalSave;
 begin
-  FFileName := AFileName;
-  Text := TPath.GetFileName(FFileName);
-  TextEditor.LoadFromFile(FFileName);
+  TextEditor.Save();
 end;
 
-procedure TEditorTabItem.SetData(const Value: TValue);
+procedure TEditorTabItem.LoadFile(const AFileName: string;
+  const AEditing: boolean);
 begin
-  FFileName := Value.AsString();
+  Text := TPath.GetFileName(AFileName);
+  TextEditor.Open(AFileName, AEditing);
 end;
 
-procedure TEditorTabItem.SetText(const Value: string);
-const
-  CLOSE_BTN_WIDTH = 30;
-var
-  LSize: TSizeF;
+procedure TEditorTabItem.DoAskToSaveChanges;
 begin
-  inherited;
-  CalcTextObjectSize(0, LSize);
-  Self.Width := LSize.cx + CLOSE_BTN_WIDTH;
+  IsSelected := true;
+  TDialogService.MessageDialog(
+    Format('Save changes to %s?', [TPath.GetFileName(TextEditor.FileName)]),
+    TMsgDlgType.mtConfirmation, [TMsgDlgBtn.mbYes, TMsgDlgBtn.mbNo], TMsgDlgBtn.mbYes, 0,
+    procedure(const AResult: TModalResult) begin
+      if (AResult = mrYes) then begin
+        TextEditor.Save();
+        TGlobalBuilderChain.BroadcastEvent(
+          TSaveStateEvent.Create(TSaveState.Save));
+      end;
+    end);
 end;
 
 procedure TEditorTabItem.DoClose;
 begin
-  TGlobalBuilderChain.BroadcastEventAsync(
-    TCloseFileEvent.Create(FFileName));
-end;
-
-function TEditorTabItem.GetCloseControl: TControl;
-begin
-  var LItemStyle: TFMXObject := nil;
-  if Assigned(ResourceLink) then
-    case TabControl.EffectiveTabPosition of
-      TTabPosition.Top:
-        LItemStyle := ResourceLink.FindStyleResource('top');
-      TTabPosition.Bottom:
-        LItemStyle := ResourceLink.FindStyleResource('bottom');
-    end;
-
-  if Assigned(LItemStyle) then
-    Result := (LItemStyle.FindStyleResource('close') as TControl)
-  else
-    Result := nil;
-end;
-
-procedure TEditorTabItem.OnCloseTab(Sender: TObject);
-begin
-  DoClose();
+  //No need to replicate changes here when closing
+  FEditorChanged.Disconnect();
+  FRenameFile.Disconnect();
+  FSaveState.Disconnect();
+  //Ask user to save editor if modified
+  if TextEditor.Modified then
+    DoAskToSaveChanges();
+  inherited;
 end;
 
 end.
