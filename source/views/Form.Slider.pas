3a2ee1b4572fee26616e9b1410c10c481679a17e
lmbelo <lucas.belo@live.com>
2023-07-28 16:49:48 -0300

    Transition was causing blur screen

--- source/views/Form.Slider.pas
+++ source/views/Form.Slider.pas
@@ -13,22 +13,15 @@ type
 
 implementation
 
+uses
+  FMX.Ani;
+
 { TFormSlider }
 
 class procedure TFormSlider.ShowModal(const AParent, AForm: TCommonCustomForm);
-const
-  PARENT_PERCENT = 0.4;
-  SLIDE_INTERVAL = 0.03;
 begin
+  var LSize := Trunc(AParent.Bounds.Width * 0.4);
   AForm.BorderIcons := [];
-  AForm.SetBounds(0, AParent.Bounds.Top, 0, AParent.Bounds.Height);
-  AForm.Show();
-  var LSize := Trunc(AParent.Bounds.Width * PARENT_PERCENT);
-  var LCurSize := 0;
-  repeat
-    AForm.SetBounds(AParent.Bounds.Right - LCurSize, AParent.Bounds.Top, LCurSize, AParent.Bounds.Height);
-    Inc(LCurSize, Trunc(LSize * SLIDE_INTERVAL));
-  until LCurSize >= LSize;
   AForm.SetBounds(AParent.Bounds.Right - LSize, AParent.Bounds.Top, LSize, AParent.Bounds.Height);
   AForm.ShowModal();
 end;
