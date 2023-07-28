6110f9b8ba631b9c1785c853e1dc88fa7ae6978a
lmbelo <lucas.belo@live.com>
2023-07-28 12:38:27 -0300

    Save text editor requester

--- source/Builder.Chain.pas
+++ source/Builder.Chain.pas
@@ -17,6 +17,7 @@ uses
 type
   {$SCOPEDENUMS ON}
   TBuilderRequest = (
+    EditorSaveRequest,
     DebuggerConnectionFrozen
   );
 
@@ -267,14 +268,17 @@ type
   TCloseFileBody = class
   private
     FFilePath: string;
+    FCheckEditing: boolean;
   public
     property FilePath: string read FFilePath write FFilePath;
+    property CheckEditing: boolean read FCheckEditing write FCheckEditing;
   end;
 
   [EventType(TBuilderEvent.CloseFile)]
   TCloseFileEvent = class(TChainEvent<TCloseFileBody>)
   public
-    constructor Create(const AFilePath: string); reintroduce;
+    constructor Create(const AFilePath: string;
+      const ACheckEditing: boolean = true); reintroduce;
   end;
 
   TRenameFileBody = class
@@ -455,6 +459,32 @@ type
   TDebuggerConnectionFrozenActionResponse = class(TChainResponse<TDebuggerConnectionFrozenActionResponseBody>)
   public
     constructor Create(const AAction: TDebuggerConnectionFrozenAction); reintroduce;
+  end;
+
+  TEditorSaveRequesBody = class
+  private
+    FTextEditor: ITextEditor;
+  public
+    property TextEditor: ITextEditor read FTextEditor write FTextEditor;
+  end;
+
+  [RequestType(TBuilderRequest.EditorSaveRequest)]
+  TEditorSaveRequest = class(TChainRequest<TEditorSaveRequesBody>)
+  public
+    constructor Create(const ATextEditor: ITextEditor); reintroduce;
+  end;
+
+  TEditorSaveResponseBody = class
+  private
+    FSavedTo: string;
+  public
+    property SavedTo: string read FSavedTo write FSavedTo;
+  end;
+
+  [RequestType(TBuilderRequest.EditorSaveRequest)]
+  TEditorSaveResponse = class(TChainResponse<TEditorSaveResponseBody>)
+  public
+    constructor Create(const ASavedTo: string); reintroduce;
   end;
 
   {|| Builder Chain ||}
@@ -839,13 +869,17 @@ begin
           Continue;
 
         var LResponse := LHandler.Notification(ARequest, LHandled);
-        if LHandled then begin
-          if LResponse.Success then
-            AResolve(LResponse as T)
-          else
-            AReject(LResponse.Message);
+        try
+          if LHandled then
+            if LResponse.Success then
+              AResolve(LResponse as T)
+            else
+              AReject(LResponse.Message);
 
           Break;
+        finally
+          if AOwned then
+            LResponse.Free();
         end;
       end;
     finally
@@ -1139,10 +1173,12 @@ end;
 
 { TCloseFileEvent }
 
-constructor TCloseFileEvent.Create(const AFilePath: string);
+constructor TCloseFileEvent.Create(const AFilePath: string;
+  const ACheckEditing: boolean);
 begin
   inherited Create();
-  FBody.FilePath := AFilePath;
+  Body.FilePath := AFilePath;
+  Body.CheckEditing := ACheckEditing;
 end;
 
 { TAsyncOperationStartedEvent }
@@ -1195,6 +1231,23 @@ begin
   inherited Create();
   Success := true;
   Body.Action := AAction;
+end;
+
+{ TEditorSaveRequest }
+
+constructor TEditorSaveRequest.Create(const ATextEditor: ITextEditor);
+begin
+  inherited Create();
+  Arguments.TextEditor := ATextEditor;
+end;
+
+{ TEditorSaveResponse }
+
+constructor TEditorSaveResponse.Create(const ASavedTo: string);
+begin
+  inherited Create();
+  Success := true;
+  Body.SavedTo := ASavedTo;
 end;
 
 { TDebugSessionStoppedEvent }
