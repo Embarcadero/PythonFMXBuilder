c68653f2f8328a66138e42287dea2009d9e1eb22
lmbelo <lucas.belo@live.com>
2023-08-10 21:08:11 -0300

    Listening to the settings update event

--- source/views/frame/Frame.BuildButtons.pas
+++ source/views/frame/Frame.BuildButtons.pas
@@ -1,21 +1,66 @@
 unit Frame.BuildButtons;
-
 interface
-
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Controls.Presentation;
+  FMX.Controls.Presentation, Builder.Messagery, Builder.SpeedButton, FMX.Menus;
 
 type
   TBuildButtonsFrame = class(TFrame)
     sbBuildCurrentProject: TSpeedButton;
     spDeployCurrentProject: TSpeedButton;
     sbRunCurrentProject: TSpeedButton;
+    pmDeployDropDown: TPopupMenu;
+    miSmartDeploy: TMenuItem;
+    procedure miSmartDeployClick(Sender: TObject);
+  private
+    FUpdateSettings: IDisconnectable;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy(); override;
   end;
 
 implementation
 
+uses
+  Container.Menu.Actions;
+
 {$R *.fmx}
+
+{ TBuildButtonsFrame }
+
+constructor TBuildButtonsFrame.Create(AOwner: TComponent);
+begin
+  inherited;
+  FUpdateSettings := TMessagery.SubscribeToEvent<TUpdateSettingsEvent>(
+    procedure(const AEventNotification: TUpdateSettingsEvent)
+    begin
+      var LSmartDeploy := AEventNotification.Body.SmartDeploy;
+      TThread.Queue(TThread.Current,
+        procedure()
+        begin
+          miSmartDeploy.IsChecked := LSmartDeploy;
+          if LSmartDeploy then begin
+            spDeployCurrentProject.Action := MenuActionsContainer.actSmartDeployCurrentProjectAsync;
+            sbRunCurrentProject.Action := MenuActionsContainer.actSmartRunCurrentProjectAsync;
+          end else begin
+            spDeployCurrentProject.Action := MenuActionsContainer.actDeployCurrentProjectAsync;
+            sbRunCurrentProject.Action := MenuActionsContainer.actRunCurrentProjectAsync;
+          end;
+        end);
+    end);
+end;
+
+destructor TBuildButtonsFrame.Destroy;
+begin
+  FUpdateSettings.Disconnect();
+  inherited;
+end;
+
+procedure TBuildButtonsFrame.miSmartDeployClick(Sender: TObject);
+begin
+  TMessagery.BroadcastEventAsync(
+    TUpdateSettingsEvent.Create(TMenuItem(Sender).IsChecked));
+end;
 
 end.
--- source/views/frame/Frame.DebugButtons.pas
+++ source/views/frame/Frame.DebugButtons.pas
@@ -5,7 +5,7 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Controls.Presentation;
+  FMX.Controls.Presentation, Builder.Messagery;
 
 type
   TDebugButtonsFrame = class(TFrame)
@@ -16,6 +16,11 @@ type
     btnStop: TSpeedButton;
     btnPause: TSpeedButton;
     sbContinue: TSpeedButton;
+  private
+    FUpdateSettings: IDisconnectable;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy(); override;
   end;
 
 implementation
@@ -25,5 +30,32 @@ uses
   Container.Images;
 
 {$R *.fmx}
+
+{ TDebugButtonsFrame }
+
+constructor TDebugButtonsFrame.Create(AOwner: TComponent);
+begin
+  inherited;
+  FUpdateSettings := TMessagery.SubscribeToEvent<TUpdateSettingsEvent>(
+    procedure(const AEventNotification: TUpdateSettingsEvent)
+    begin
+      var LSmartDeploy := AEventNotification.Body.SmartDeploy;
+      TThread.Queue(TThread.Current,
+        procedure()
+        begin
+          if LSmartDeploy then begin
+            sbDebugCurrentProject.Action := MenuActionsContainer.actSmartDebugCurrentProjectAsync;
+          end else begin
+            sbDebugCurrentProject.Action := MenuActionsContainer.actDebugCurrentProjectAsync;
+          end;
+        end);
+    end);
+end;
+
+destructor TDebugButtonsFrame.Destroy;
+begin
+  FUpdateSettings.Disconnect();
+  inherited;
+end;
 
 end.
