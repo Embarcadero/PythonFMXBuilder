9fa2182c38d7cf8cdd03953a1b40d79f096e8eed
lmbelo <lucas.belo@live.com>
2023-09-30 15:24:16 -0300

    Inverting image to white

--- source/views/Form.Environment.pas
+++ source/views/Form.Environment.pas
@@ -7,7 +7,8 @@ uses
   System.Threading, System.Generics.Collections, FMX.Types, FMX.Controls, FMX.Forms,
   FMX.Graphics, FMX.Dialogs, FMX.StdCtrls, FMX.Objects, FMX.ListBox, FMX.Ani,
   FMX.Controls.Presentation, FMX.Edit, FMX.Layouts, FMX.ImgList, System.Actions,
-  FMX.ActnList, Form.Data, Builder.Model.Environment;
+  FMX.ActnList, Form.Data, Builder.Model.Environment, FMX.Effects,
+  FMX.Filter.Effects;
 
 type
   [Entity(TEnvironmentModel)]
