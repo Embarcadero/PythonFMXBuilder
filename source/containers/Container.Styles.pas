003f5378033f2896fb2a93917de351c063a4f9df
lmbelo <lucas.belo@live.com>
2023-09-30 15:41:31 -0300

    Adding the Material Oxford Blue style

---
+++ source/containers/Container.Styles.pas
@@ -0,0 +1,28 @@
+unit Container.Styles;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, FMX.Types, FMX.Controls,
+  //MaterialOxforBlueSB make uses of TInvertEffect - The effects unit must be used here!
+  FMX.Effects, FMX.Filter.Effects, REST.Backend.KinveyProvider;
+
+type
+  TStyleContainer = class(TDataModule)
+    MaterialOxfordBlueSB: TStyleBook;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  StyleContainer: TStyleContainer;
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
